05e7f42261c8c8aee1ba2a29a683fde37f0d6b7a
ziggysauce <dtn3423@gmail.com>
2019-04-20 11:23:44 -0400

    setup events api

---
+++ src/api/Events.js
@@ -0,0 +1,19 @@
+/*
+ * Example of an event object
+ * All of the information needs to be formatted the same way
+  {
+    date: "June 15",
+    time: "9:00 PM",
+    location: "Gypsy Sally's (Washington, D.C.)",
+  }
+ */
+
+module.exports = {
+  events: [
+    {
+      date: "June 15",
+      time: "9:00 PM",
+      location: "Gypsy Sally's (Washington, D.C.)",
+    },
+  ]
+};
--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import EventsAPI from '../api/Events';
 
 class Events extends Component {
+  state = { events: EventsAPI.events }
+
+  componentWillMount() {
+    this.setState({ events: EventsAPI.events });
+  }
+
   render() {
     return (
       <div id="events-section" className="min-vh-100 w-100 bg-dark text-light d-flex justify-content-around">
         <p>Events Section</p>
+        {this.state.events.map(({date, location, time}, idx) => {
+          return (
+            <div key={idx} className="row">
+              <div class="col-6 text-right d-flex flex-column">
+                <p class="mb-0">{date}</p>
+                <p class="mb-0">{time}</p>
+              </div>
+              <p class="col-6 text-left mb-0">{location}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
