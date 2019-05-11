9fd059ea71509551e1d2c887062490f0e0943c54
ziggysauce <dtn3423@gmail.com>
2019-05-11 06:02:42 -0400

    video section carousel

--- src/pages/Merch.js
+++ src/pages/Merch.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Merch extends Component {
-  render() {
-    return (
-      <div id="merch-section" className="min-vh w-100 bg-salmon text-light d-flex justify-content-around">
-        <p>Merch Section</p>
-      </div>
-    );
-  }
+const Merch = () => {
+  return (
+    <div id="merch-section" className="min-vh w-100 bg-salmon text-light d-flex justify-content-around">
+      <p>Merch Section</p>
+    </div>
+  );
 }
 
 export default Merch;
--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 
-const Video = () => {
-  return (
-    <div id="video-section" className="min-vh w-100 bg-salmon text-light d-flex justify-content-around">
-      <iframe src="https://www.youtube.com/embed/fCnCZvr2S7s"
-        className="w-100"
-        title="Anjali YouTube Video"
-        frameBorder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen>
-      </iframe>
-    </div>
-  );
+class Video extends Component {
+  state = {
+    index: 0,
+    direction: null,
+  };
+
+  handleSelect = (selectedIndex, e) => {
+    this.setState({
+      index: selectedIndex,
+      direction: e.direction,
+    });
+  }
+
+  render() {
+    const { index, direction } = this.state;
+    return (
+      <Carousel
+        activeIndex={index}
+        direction={direction}
+        interval={null}
+        onSelect={this.handleSelect}>
+        <Carousel.Item className="bg-black">
+          <iframe src="https://www.youtube.com/embed/fTT4OK_OrnA"
+            className="w-100 min-vh mb-5"
+            title="Anjali Taneja x Pav Dharia YouTube Video"
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen>
+          </iframe>
+        </Carousel.Item>
+        <Carousel.Item className="bg-black">
+          <iframe src="https://www.youtube.com/embed/fCnCZvr2S7s"
+            className="w-100 min-vh mb-5"
+            title="Anjali Taneja YouTube Video"
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen>
+          </iframe>
+        </Carousel.Item>
+      </Carousel>
+    );
+  }
 }
 
 export default Video;
