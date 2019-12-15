e36bde3087edc284c25403698b78a02d13c4af18
Anjali <anjalitaneja94@gmail.com>
2019-12-15 13:36:38 -0500

    Updates

---
+++ precache-manifest.d7c1fbb73987cb114d0e9ce4e761a248.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "abafbfb41d729ffa8ca4",
+    "url": "/static/css/main.c06f5a32.chunk.css"
+  },
+  {
+    "revision": "abafbfb41d729ffa8ca4",
+    "url": "/static/js/main.bcb55908.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "421e1a602b5052ca2cbb",
+    "url": "/static/css/2.f2273868.chunk.css"
+  },
+  {
+    "revision": "421e1a602b5052ca2cbb",
+    "url": "/static/js/2.ed2d89a5.chunk.js"
+  },
+  {
+    "revision": "b8f9b6d98786c39e3a78d53c1fc8ac2f",
+    "url": "/static/media/at_landing.b8f9b6d9.jpg"
+  },
+  {
+    "revision": "b2491b5ca7b7760795dcb98e7f3d2c4a",
+    "url": "/static/media/at_music.b2491b5c.jpg"
+  },
+  {
+    "revision": "944386b5d8d93f950244d304b5e7cabe",
+    "url": "/static/media/at_events.944386b5.jpg"
+  },
+  {
+    "revision": "73399674dd5eccb626da63679356d4ab",
+    "url": "/static/media/at_contact.73399674.jpg"
+  },
+  {
+    "revision": "19f1f3cbbc5cdc02928a5a3e4452f1bd",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c4f00c68738b2e87139e382224168bce.js"
+  "/precache-manifest.d7c1fbb73987cb114d0e9ce4e761a248.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
